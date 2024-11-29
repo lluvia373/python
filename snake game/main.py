97e0e6a1bde048349a2bf8399f273e93b2770664
lluvia373 <lluvia7531@gmail.com>
2024-11-29 15:53:15 +0900

    Add Highscore and Shortcut key

--- snake game/main.py
+++ snake game/main.py
@@ -8,40 +8,53 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=600, height=600)
 screen.title("Snake Game")
-screen.tracer(0)
-
+screen.tracer(0.1)
 
 game_is_on = True
+is_paused = False
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
+def game_pause():
+    global is_paused
+    is_paused = not is_paused
+def game_quit():
+    global game_is_on
+    game_is_on = False
+    screen.bye()
+    raise SystemExit
 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+screen.onkey(game_pause, "space")
+screen.onkey(game_quit, "Escape")
+
 
 while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    if not is_paused:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    if snake.head.distance(food) < 13:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+        if snake.head.distance(food) < 13:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
-
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-
-        elif snake.head.distance(segment) < 10:
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             game_is_on = False
             scoreboard.game_over()
 
-screen.exitonclick()
+        for segment in snake.segments:
+            if segment == snake.head:
+                pass
+
+            elif snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+    else:
+        screen.update()
+
--- snake game/scoreboard.py
+++ snake game/scoreboard.py
@@ -1,17 +1,20 @@
 from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
+HiGH_SCORE_FILE = "highscore.txt"
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = self.load_highscore()
         self.penup()
         self.color("white")
         self.goto(0,270)
         self.hideturtle()
         self.update_scoreboard()
+
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} HighScore: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0,0)
@@ -19,4 +22,17 @@ class Scoreboard(Turtle):
     def increase_score(self):
         self.score += 1
         self.clear()
+        if self.score > self.highscore:
+            self.highscore = self.score
         self.update_scoreboard()
+        self.save_highscore()
+    def load_highscore(self):
+        try:
+            with open(HiGH_SCORE_FILE, "r") as file:
+                return int(file.read())
+        except FileNotFoundError:
+            return 0 # try except 구문 설명
+
+    def save_highscore(self):
+        with open(HiGH_SCORE_FILE, "w") as file:
+            file.write(str(self.highscore))
